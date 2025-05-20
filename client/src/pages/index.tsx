f1aaabeeb0bdf69fbbbe3b6527cc333aa00d281d
Mason <masoncfrancis@gmail.com>
2025-05-20 12:29:02 -0400

    Added auth0 stuff

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,15 @@
 import GasLogForm from "@/components/GasLogForm";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 
 export default function Home() {
   return (
-    <div>
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI}
+      >
       <GasLogForm />
-    </div>
+    </Auth0Provider>
   );
 }
